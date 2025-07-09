b4160d3e4969428254694e01f8788b93d32cd33d
Aman Kumar <rohitamanrazz@gmail.com>
2025-07-09 14:15:12 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+import connectDB from "./config/db.js";
+import authRouter from "./routes/auth.routes.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+const app = express();
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cookieParser());
+app.use("/api/auth", authRouter);
+app.listen(port, () => {
+  connectDB();
+  console.log("server is running");
+});
