af846f31ecc2716074ec2244d57695259d81ea05
Aman Kumar <rohitamanrazz@gmail.com>
2025-07-10 13:08:23 +0530

    Added Cloudinary multer some addition in userContex

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,8 +1,7 @@
 import express from "express";
+import isAuth from "../middlewares/isAuth.js";
 import { getCurretnUser } from "../controllers/user.controllers.js";
-
 const userRouter = express.Router();
 
-
-userRouter.get("/current", getCurretnUser)
+userRouter.get("/current", isAuth, getCurretnUser);
 export default userRouter;
--- frontend/src/context/UserContext.jsx
+++ frontend/src/context/UserContext.jsx
@@ -1,7 +1,24 @@
-import React, { createContext } from 'react'
+import React, { createContext, useEffect, useState } from 'react'
+import axios from 'axios';
 export const UserDataContext = createContext();
 function UserContext({ children }) {
     const serverUrl = "http://localhost:8000";
+    const [userData, setUerData] = useState(null);
+
+    const handleCurrentUer = async () => {
+        try {
+            const result = await axios.get(`${serverUrl}/api/user/current`, { withCredentials: true });
+            setUerData(result.data);
+            console.log(result.data);
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    useEffect(() => {
+        handleCurrentUer();
+    },[])
+
     const value = {
         serverUrl
     }
