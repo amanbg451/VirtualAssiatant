5630c54ff4a8c7d6599120c53ce0678716c38b1b
Aman Kumar <rohitamanrazz@gmail.com>
2025-07-14 19:45:40 +0530

    Changes in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,11 @@
-import React, { useContext } from 'react'
+import Home from './pages/Home'
 import SignUp from './pages/SignUp'
 import SignIn from './pages/SignIn'
-import { Routes, Route, Navigate } from 'react-router-dom'
+import React, { useContext } from 'react'
 import Customize from './pages/Customize'
-import Home from './pages/Home'
-import { UserDataContext } from './context/userContext'
 import Customize2 from './pages/Customize2'
+import { UserDataContext } from './context/userContext'
+import { Routes, Route, Navigate } from 'react-router-dom'
 
 const App = () => {
   const { userData, setUserData } = useContext(UserDataContext)
@@ -15,8 +15,8 @@ const App = () => {
         <Route path="/" element={(userData?.assistantImage && userData?.assistantName) ? <Home /> : <Navigate to={"/customize"} />} />
         <Route path="/signup" element={!userData ? <SignUp /> : <Navigate to={"/"} />} />
         <Route path="/signin" element={!userData ? <SignIn /> : <Navigate to={"/"} />} />
-        <Route path="/customize" element={ userData ? <Customize /> : <Navigate to={"/signin"} />} />
-        <Route path="/customize2" element={ userData ? <Customize2 /> : <Navigate to={"/signin"} />} />
+        <Route path="/customize" element={userData ? <Customize /> : <Navigate to={"/signin"} />} />
+        <Route path="/customize2" element={userData ? <Customize2 /> : <Navigate to={"/signin"} />} />
 
       </Routes>
     </>
